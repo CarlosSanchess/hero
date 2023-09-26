7247dae2a31b967ca2f7d022d86b2aedce3d3bcf
Cruddle <up202107694@fe.up.pt>
2023-09-26 22:21:12 +0100

    Char on screen

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com.Carlos.hero"
@@ -12,8 +13,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.googlecode.lanterna:lanterna:3.1.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+application{
+    mainClass.set("com.Carlos.hero/Application")
 }
